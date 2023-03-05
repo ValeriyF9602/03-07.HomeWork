d77c469097795f922e0f395c32b94eea35440ee2
Valeriy Fateev <Valerka9602@mail.ru>
2023-03-05 15:34:35 +0800

    Решение задачи 47

--- task47/Program.cs
+++ task47/Program.cs
@@ -11,3 +11,42 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 
 ********************************************************/
+
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] GenerateMatrix(int rows, int cols, double leftRange, double rightRange)
+{
+    double[,] matrix = new double[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Math.Round(rand.NextDouble() * (rightRange - leftRange) + leftRange, rand.Next(0, 2));
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write(matrix[i,j] + "\t"); 
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int m = ReadInt("Укажите количество строк матрицы: ");
+int n = ReadInt("Укажите количество столбцов матрицы: ");
+double[,] myMatrix = GenerateMatrix(m, n, -1, 1);
+PrintMatrix(myMatrix);
